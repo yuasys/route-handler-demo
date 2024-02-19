bd54eb7cdcbc6ccc2e2ca0675b96427e700b44c8
Nob Yuasys <yuasys@gmail.com>
2024-02-20 07:09:10 +0900

    Update README.md and route.ts*** ***README.mdとroute.tsを更新

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET() {
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers()
+  
+  console.log(requestHeaders.get("authorization"));
+  console.log(headerList.get("authorization"));
+
   return new Response("Profile API data");
 }

890cb0e97ea68fa0f33b6da1690a92c7b604418c
Nob Yuasys <yuasys@gmail.com>
2024-02-19 20:33:38 +0900

    プロファイルAPIの変更を追加

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response("Profile API data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  console.log(requestHeaders.get("authorization"));
+  console.log(headerList.get("authorization"));
+
+  return new Response("<h1>Profile API data</h1>",{
+    headers: {
+      "content-type": "text/html"
+    }
+  });
 }

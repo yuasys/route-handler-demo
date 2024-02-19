f7fbe66832b4f73d7eff0e699c6275aaa69423f3
Nob Yuasys <yuasys@gmail.com>
2024-02-19 14:36:23 +0900

    コメントのPOSTリクエストを追加***

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -4,6 +4,15 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text 
+  }
+  comments.push(newComment);
+  return new Response("GET handler");
+}
 
 // export async function GET(request: NextRequest) {
 //   const searchParams = request.nextUrl.searchParams;
